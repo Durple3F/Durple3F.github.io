cf0712d8ffa952c0d1502d27dc2fa89ce7340ff1
brianhob-cwhq <brianhob@codewizardshq.com>
2023-12-14 18:16:42 -0500

    mtg packs and lojban

---
+++ lojban/data.js
@@ -0,0 +1,23 @@
+const storyInfo = {
+  "MKM-1": {
+    1: "\"ni'o\" just means \"we're talking about something new now.\" it's not really necessary for speech, but lojban doesn't <i>technically</i> allow you to imply new paragraphs with an indent like we do in english.",
+    2: "\"le\" is somewhat analagous to english \"the\"",
+    3: "\"noi\" means \"for which (the thing after 'noi') is incidentally true\" so this means, \"which happened to be-over-Karlov-Manor\"",
+    4: "Another perfectly acceptable way to translate this would be \"la .karlov.manor.\" but that imples that Manor is just the <i>name</i> of the thing, it's not a manor of whatever a Karlov is.",
+    5: "Weirdly, \"tsali\" just means it is an <i>expanse</i> of sky, not the whole sphere around the planet. The definitions here are very specific.",
+    6: "This little \"ki\" right here is what causes the rest of the text to be in past tense. Because it's right after the word \"pu,\" meaning \"past tense,\" it causes that tense to be the default until something says otherwise.",
+    7: "Past tense",
+    8: "\"cu\" means (approximately) \"I'm done telling you the subject of this part of the sentence now, and next is the actual verb\"",
+    9: "\"dansu\" is one of few examples of a verb that sounds like what it means. Those do not happen often.",
+    10: "The sky is not actually really physically dancing. This is an indicator for that. There's a lot of tone indicators in this language, which may not be surprising.",
+    11: "\"and\" here in the same sense as you might say \"the boy jumped and ran\" to allow you to combine two predicates that share a single subject",
+    12: "I'm not happy with having to split the sentence up like this. The second best thing you can do is say \"it danced such that the property of it containing colors... is true\" which just sounds way worse.",
+    13: "This is like if we had a number that meant \"lots.\" I guess we do, and it's called \"lots\" or \"many\" but it is explicitly treated as a number in this language.",
+    14: "This is color as in the physical thing, and not the idea of a color, which is its own separate word.",
+    15: "There's not a lot of words that are as evocative as I'd like.",
+    16: "\"ri'a\" means \"is physically caused by\" and it's just one of a very long list of ways-things-can-make-things-happen. I suppose in this context it's less physically-caused-by and more supernaturally-caused-by, but sadly, that's not on the list.",
+    17: "Perhaps the most common word in the language, \"lo\" means \"turn this verb into a noun\" which weird as hell. Like, imagine if you could just take a word like \"run\" and turn it into \"a runner\" and that was possible for any concept the language could express. It works in reverse, too, you can take \"a cat\" and turn it into \"cat (verb)\" (that word is \"mlatu\")",
+    18: "Let me tell you, after spending as much time as I have thinking about ways to say different magic concepts in this shit language, we are spoiled for choice when it comes to english. Witchcraft, enchantment, artifice, spellweaving, alchemy, we've got plenty of words. All of their equivalents in lojban are gonna have a \"makfa\" in there.",
+    19: "Cascade is such a weird word. What is a \"cascade\"? Can you think of a single, platonic ideal of what a \"cascade\" would be? Not a cascade of anything in particular, just in general. It's like a wave, right?"
+  }
+}
---
+++ lojban/main.js
@@ -0,0 +1,104 @@
+let allSpans = $(".col span")
+const story = storyInfo[storyCode]
+const storyElemValues = {}
+const storyColors = {}
+const myrng = new Math.seedrandom(storyCode);
+
+let colors = [
+  "red", "crimson", "firebrick", "coral",
+  "orange", "orangered", "darkorange", "yellow", "gold",
+  "chartreuse", "green", "lime", "mediumspringgreen", "seagreen", "forestgreen",
+  "blue", "blueviolet", "deepskyblue",
+  "magenta", "fuchsia", "indigo", "orchid", "rebeccapurple"
+]
+let colorUsage = {}
+for (let color of colors){
+  colorUsage[color] = 0
+}
+
+function getValsFromElement(element){
+  for (let factCode in storyElemValues){
+    let vals = storyElemValues[factCode]
+    let elems = vals.elems
+    for (let i = 0; i < elems.length; i++){
+      let e = elems[i]
+      if (e === element){
+        return vals
+      }
+    }
+  }
+}
+
+let currentlyHovered;
+function hoverFade(e){
+  let hoveredList = document.elementsFromPoint(e.clientX, e.clientY)
+  let hovered
+  for (let tag of hoveredList){
+    if (tag.tagName === "SPAN"){
+      hovered = tag
+      break
+    }
+  }
+  if (!hovered){
+    return
+  }
+  if (currentlyHovered){
+    unHoverFade(currentlyHovered)
+  }
+
+  currentlyHovered = hovered
+  let vals = getValsFromElement(hovered)
+  if (!vals) {
+    console.warn("????", hovered)
+  }
+  let css = {
+    "background-color": vals.color,
+    "text-shadow": "0px 0px 2px black"
+  }
+  vals.elems.css(css)
+  $("#explanation").html(vals.fact)
+}
+function unHoverFade(e){
+  if (!e.textContent){
+    e = $(e.delegateTarget)[0]
+  }
+  let vals = getValsFromElement(e)
+  if (!vals) {
+    console.warn("????", hovered)
+  }
+  let css = {
+    "background-color": "transparent",
+    "text-shadow": "inherit"
+  }
+  vals.elems.css(css)
+  $("#explanation").html("")
+}
+
+for (let factCode in story){
+  let fact = `<div class="fact">${story[factCode] ?? ""}</div>`
+  let className = `.f${factCode}`
+  let thoseSpans = $(className)
+  let minColorUsage = Math.min(...Object.values(colorUsage))
+  let validColors = colors.filter(c => colorUsage[c] === minColorUsage)
+  let color = validColors[Math.floor(myrng.quick() * validColors.length)]
+  colorUsage[color]++
+  storyColors[factCode] = color
+  let css = {
+    "text-decoration-color": color,
+    "text-decoration-style": "solid"
+  }
+  let boundObj = {
+    elems: thoseSpans,
+    color: color,
+    fact: fact
+  }
+  storyElemValues[factCode] = boundObj
+  thoseSpans.css(css)
+  .on("mousemove", hoverFade)
+  .on("mouseleave", unHoverFade)
+
+  let children = thoseSpans.children("span")
+  for (let child of children){
+    $(child).css("border-bottom", "1px solid "+color)
+  }
+}
