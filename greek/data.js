a9960268c45701224b56a8b20ba15cd3110b8203
brianhob-cwhq <brianhob@codewizardshq.com>
2024-03-20 00:10:54 -0400

    lightning bolt greek

---
+++ greek/data.js
@@ -0,0 +1,27 @@
+const storyInfo = {
+	"general": {
+		
+	},
+	"Lightning Bolt": {
+		1: "Hey, would you say that naming a card like this is the subject or object of a sentence? What, you don't think this counts as a sentence? Wrong. Greek needs to know. It seems like this should be what they call the 'nominative' case, which is the most common one.",
+		2: "'Astrapí' here means lightning, or flash.",
+		3: "This is actually the same 'bolos' that 'bola' (the weapons that you throw) comes from.",
+		4: "This is a very general word, that could mean all sorts of causal things. I suppose you could also use a word for like 'give'? Isn't it weird how we use the word 'deal' in this context? Why do you give people damage?",
+		5: "I <i>think</i> damage is supposed to be singular here. I have no idea.",
+		6: "Se can mean damn near any kind of 'to' in Greek. In, on, to, at, most conjunctions.",
+		7: "Maybe I'm just missing a better word, but 'whatever' certainly fits the bill.",
+		8: "Apparently this only means 'target' in archery contexts, but 'target' also doesn't mean target in most contexts in English. This word can mean like 'aim' or 'goal' which is... just like English.",
+		9: "Greek just sticks a Δεν in front of anything that ain't.",
+		10: "Greek is very conjugated. The 'έχ' part means 'have' and the 'ει' part means 'it is a singular person who is doing the having'. With nouns, the gender matters, and it would probably be more accurate for me to stick a female pronoun like 'αυτή' after this word.",
+		11: "Hey, look at that, the word for hearing sounds like 'acoustic'.",
+		12: "This is what we in the business call a 'subjunctive'. That's the fun term for clauses-within-sentences like this. This is a pretty simple one, but in English, you see this kind of thing whenever you say \"I wish that <wish goes here.>\" Isn't that weird, that the wish can just be its own whole sentence? \"I wish I were taller.\" In English, we have an entirely new way of conjugating \"be\" to allow those clauses to be less confusing. In Greek, you get to see the \"να\" and figure it out.",
+		13: "I want you to try and say this word. The 'ch' sounds like a cat hissing, and the 'τ' just makes a t sound. So say it, go 'chti-pa-ei'!",
+		14: "I cannot stress enough that the Greek question mark is a FUCKING semicolon. Why?? As best I can tell, we should blame the Salvonic Church in particular.",
+		15: "I <i>believe</i> this attribution here calls for the 'genitive' case, usually used for portraying relationships between things. The quote has a relationship with its speaker, right? Anyway, that means we gotta stick an O in front of Snerd's name. It would be an H if Snerd were a girl, and maybe they are, but O is the default.",
+		16: "I want to really draw your attention to the fact that this ends with an 'ντ'. In Modern Greek, the δ only makes a very soft d sound, closer to the 'th' in 'the'. So our good buddy Snerd here, with a hard d at the end of their name, must simply be okay being called Snernt. How sad. Fun fact, in Greek, 'Avocado' is basically spelled 'Abokanto' for the same reason. It uses a beta! But not a delta!",
+		17: "I am <i>very</i> not confident about my usage of this word. Is it okay to use this as an adjective? Probably. What about as only a <i>piece</i> of an adjective that's being used as an epithet? I dunno! I assume that the capital letter makes it pretty clear this is a proper noun!",
+		18: "This one is even weirder! It's a <i>noun</i>! Like, morphologically! I'm really hoping it's fine to just stick a noun in front of a noun like this. All the examples I can find are things that make a lot more sense as adjectives, like leather or wool, but are also used as nouns.",
+		19: "This one I'm pretty confident with. Sure, I accidentally ended up using the ancient greek version of the word for half of this project, but I figured it out.",
+		20: "I'm really unsure whether this language is OK with you just saying it deals 3 of 'a concept' to something. Gonna play it safe."
+	}
+}
